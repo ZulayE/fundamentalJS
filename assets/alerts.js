20217fd265c127b1ebc0fd1829100a488119c278
Zulay Esparza <zulay.esparza@gmail.com>
2023-01-12 19:03:46 -0600

    JS

---
+++ assets/alerts.js
@@ -0,0 +1,10 @@
+//Bloque la ejecución del navegador web
+
+alert('Hola Mundo')
+
+//Nos permite recibir valor e introducir a una variable
+let nombre=prompt('¿Comote llamas?')
+console.log(nombre) //'', null
+
+//Nos permite interactuar con respuestas de usuario
+const valor=confirm('¿Seguro que te llamas: '+nombre+'?') //'', null
---
+++ assets/ejercicio01.js
@@ -0,0 +1,11 @@
+/*let num1=prompt('¿Dame un numero entero?')
+let num2=prompt('¿Dame otro numero enteros?')
+let suma=parseInt (num1) +parseInt(num2)
+alert(suma)*/
+//console.log(suma)
+
+let num4=prompt('¿Dame un numero decimales?')
+let num5=prompt('¿Dame otro numero decimales?')
+let suma1=parseFloat (num4) + parseFloat(num5)
+document.write(suma1)
+alert(suma1)
